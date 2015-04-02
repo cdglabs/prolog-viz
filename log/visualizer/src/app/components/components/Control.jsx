d37af688804da3398ea073e6b2c6b217d8f4321d
Zhixuan Lai <zhxnlai@gmail.com>
2015-04-02 16:46:58 -0700

    replacing flat button with icon button

--- log/visualizer/src/app/components/components/Control.jsx
+++ log/visualizer/src/app/components/components/Control.jsx
@@ -4,7 +4,9 @@ var Slider = mui.Slider;
 var FlatButton = mui.FlatButton;
 var React = require('react');
 var Classable = require('../../mixins/classable.js');
-
+var IconButton = mui.IconButton;
+var ForwardIcon = require('../components/ForwardIcon.jsx');
+var BackwardIcon = require('../components/BackwardIcon.jsx');
 var assign = require('object-assign');
 
 var CodeMirror = require('react-code-mirror');
@@ -104,6 +106,23 @@ var Control = React.createClass({
       sliderProps.value = step;
     }
 
+    var forward = <IconButton className="right" onTouchTap={this.forward}>
+            <ForwardIcon/>
+            </IconButton>
+
+            // <FlatButton className="left" label="Back" onTouchTap={this.backward}/>
+
+    var back = <IconButton className="left" onTouchTap={this.backward}>
+            <BackwardIcon/>
+            </IconButton>
+
+    // var back = <BackwardIcon
+    //         className="left"
+    //         onTouchTap={this.backward}/>
+
+            // <FlatButton className="right" label="Forward" onTouchTap={this.forward}/>
+
+
     return (
       <div className={classes}>
         <div className="toggle">
@@ -114,9 +133,7 @@ var Control = React.createClass({
           <Slider name="slider1" {...sliderProps}/>
         </div>
         <div className="buttons">
-          <FlatButton className="left" label="Back" onTouchTap={this.backward}/>
-          <p className="mid">Step {step+1} of {maxStep+1}</p>
-          <FlatButton className="right" label="Forward" onTouchTap={this.forward}/>
+          {back}<p className="mid">Step {step+1} of {maxStep+1}</p>{forward}
         </div>
       </div>
       );
